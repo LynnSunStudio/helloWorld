9efe1736aa1670d44a2f942e13017df35e1c3127
Administrator <sunlin@minke.cn>
2019-05-29 16:39:05 +0800

    添加GoodBye

--- src/main/java/com/lynnsunstudio/test/controller/HelloWorldController.java
+++ src/main/java/com/lynnsunstudio/test/controller/HelloWorldController.java
@@ -1,7 +1,6 @@
 package com.lynnsunstudio.test.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,4 +18,10 @@ public class HelloWorldController {
     public String say(){
         return "Hello World ！！！";
     }
+
+    @GetMapping("/sayGoodBye")
+    public String goodBye(){
+        return "GoodBye World ！！！";
+    }
+
 }
